d1d2055314b2a1851ff1f5d8a6c27483ff980f18
Remy Sanlaville <35846247+mobbarley78110@users.noreply.github.com>
2024-05-27 23:13:38 -0400

    added path

--- scripts/Building.gd
+++ scripts/Building.gd
@@ -1,27 +1,27 @@
 extends TileMap
-@onready var cursor = $"../UI/Cursor"
-@onready var mouse_pos_label = $"../UI/VBoxContainer/Mouse Pos Label"
-@onready var tile_pos_label = $"../UI/VBoxContainer/Tile Pos Label"
-@onready var camera_2d = $"../Player/Camera2D"
+
 var grid_size: int
 
 func _ready():
-	cursor.visible = false
 	grid_size = self.rendering_quadrant_size
 
 func _physics_process(_delta):
 	# display white cursor where player can walk
-	var mouse_pos = get_viewport().get_mouse_position()
-	var tile_pos = local_to_map(mouse_pos)
-	var tile_data = get_cell_tile_data(0, tile_pos)
-	if tile_data is TileData:
-		if tile_data.get_custom_data("walkable") == true && tile_data.get_custom_data("is_wall") == false:
-			# display little square
-			mouse_pos_label.text = str(mouse_pos.x) + ", " +  str(mouse_pos.y)
-			tile_pos_label.text = str(tile_pos.x) + ", " +  str(tile_pos.y) 
-			cursor.position = map_to_local(tile_pos)# - Vector2(grid_size,grid_size)
-			cursor.visible = true
-		else:
-			cursor.visible = false
-		
-		#cursor.visible = true
+	var mouse_pos = get_local_mouse_position() # position of the mouse on the grid
+	var tile_pos = local_to_map(mouse_pos) # tile x,y coords on the grid
+	var tile_data_floor = get_cell_tile_data(0, tile_pos) # data of that tile
+	var tile_data_wall = get_cell_tile_data(1, tile_pos) # data of that tile
+	clear_layer(3)
+	# find out rules for walkable and is_wall
+	var a :bool = false
+	var b :bool = false
+	var can_go :bool = false
+	if tile_data_floor is TileData:
+		a = tile_data_floor.get_custom_data("walkable")
+	if tile_data_wall is TileData:
+		b = tile_data_wall.get_custom_data("is_wall")
+	can_go = a and !b
+	if can_go:
+		set_cell(3, tile_pos, 0, Vector2(67,1), 0)
+	if not(can_go):
+		set_cell(3, tile_pos, 0, Vector2(69,1), 0)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,51 +1,80 @@
 extends CharacterBody2D
 
-@onready var building = $"../Building"
+@onready var tile_set = $"../Building"
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 var astar_grid: AStarGrid2D
-var current_id_path: Array[Vector2i]
+var selected_path: Array[Vector2i]
+var available_path: Array[Vector2i]
 var speed = 2
 
 func _ready():
 	astar_grid = AStarGrid2D.new()
-	astar_grid.region = building.get_used_rect()
+	astar_grid.region = tile_set.get_used_rect()
 	astar_grid.cell_size = Vector2(16,16)
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ALWAYS
 	astar_grid.update()
 	
-	for x in building.get_used_rect().size.x:
-		for y in building.get_used_rect().size.y:
+	# iterate over all tiles to see which ones are walkable 
+	# so they can be added to the a* "good tiles to use" 
+	#TODO: add weight for slow moving tiles if exist
+	for x in tile_set.get_used_rect().size.x:
+		for y in tile_set.get_used_rect().size.y:
 			var tile_position = Vector2i(
-				x + building.get_used_rect().position.x,
-				y + building.get_used_rect().position.y)
-			var tile_data = building.get_cell_tile_data(0, tile_position)
-			var tile_data2 = building.get_cell_tile_data(1, tile_position)
-			if tile_data2 == null:
-				if tile_data == null or tile_data.get_custom_data("walkable") == false:
-					astar_grid.set_point_solid(tile_position)
-			else: 
-				if tile_data == null or tile_data.get_custom_data("walkable") == false or tile_data2.get_custom_data("is_wall"):
-					astar_grid.set_point_solid(tile_position)
+				x + tile_set.get_used_rect().position.x,
+				y + tile_set.get_used_rect().position.y)
+			var tile_data_floor = tile_set.get_cell_tile_data(0, tile_position)
+			var tile_data_wall = tile_set.get_cell_tile_data(1, tile_position)
+			# test if the tile is walkable
+			var can_walk: bool = false
+			var a: bool = false
+			var b: bool = false
+			if tile_data_floor:
+				if tile_data_floor.get_custom_data("walkable") == true:
+					a = true
+			if tile_data_wall:
+				if tile_data_wall.get_custom_data("is_wall"):
+					b = true
+			can_walk = a and not(b)
+			if !can_walk:
+				astar_grid.set_point_solid(tile_position)
 
 func _input(event):
-	if event.is_action_pressed("left_click") == false:
-		return
-	
-	var id_path = astar_grid.get_id_path(
-		building.local_to_map(global_position),
-		building.local_to_map(get_global_mouse_position())
-	).slice(1)
-	
-	if id_path .is_empty() == false: 
-		current_id_path = id_path
+	# if user **clicks on a good walkable tile
+	if event.is_action_pressed("left_click") == true:
+		selected_path = find_path()
 		
+	#TODO: if user uses keyboard to move (continuous movement)
+	
 func _physics_process(delta):
-	if current_id_path.is_empty() == true:
-		return
-	
-	var target_position = building.map_to_local(current_id_path.front())
-	global_position = global_position.move_toward(target_position, speed) 
-	
-	if global_position == target_position:
-		current_id_path.pop_front()
+
+	# logic to move the player if a path was clicked
+	if selected_path.is_empty() == false:
+		# animate the player
+		animated_sprite_2d.play('run')
+		
+		# move the player to the next step in the a* array
+		var target_position = tile_set.map_to_local(selected_path.front())
+		global_position = global_position.move_toward(target_position, speed) 		
+		if global_position == target_position:
+			selected_path.pop_front()
+	else:
+		animated_sprite_2d.play('idle')
+			# get position of mouse and show an a* path if exist
+		available_path = find_path()
+		for p in available_path:
+			# draw a little blob on each tile of the path
+			tile_set.set_cell(3, p, 1, Vector2(4,0), 0)
+
+
+func find_path() -> Array[Vector2i]:
+	var _rez:Array[Vector2i]
+	var id_path = astar_grid.get_id_path(
+			tile_set.local_to_map(global_position),
+			tile_set.local_to_map(get_global_mouse_position())
+		).slice(1) # Remove the first element of the path (the current position)
+	if id_path .is_empty() == false: 
+		return id_path
+	else: 
+		return _rez
 
